722344bd4f2d0d3e22063f9da3c7d5a2e86393ce
macos <macos@macosdemac.local>
2017-12-29 11:55:11 +0800

    Initial Commit

---
+++ iCheck/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iCheck
+//
+//  Created by macos on 2017/12/29.
+//  Copyright © 2017年 macos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iCheck/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iCheck
+//
+//  Created by macos on 2017/12/29.
+//  Copyright © 2017年 macos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
